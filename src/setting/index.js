8915389e40a2e6bb15f527bdf2c00d61235666dd
bluelovers <codelovers@users.sourceforge.net>
2019-01-26 23:40:11 +0800

    .

---
+++ src/setting/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * Created by user on 2019/1/26/026.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProjectSetting = Object.freeze({
+    //FETCH_API: 'https://gitee.com/bluelovers/novel/raw/master/novel-stat.json',
+    FETCH_API: 'https://gitlab.com/novel-group/txt-source/raw/master/novel-stat.json',
+    //NOVEL_LINK: 'https://gitee.com/bluelovers/novel/tree/master/',
+    NOVEL_LINK: 'https://gitlab.com/novel-group/txt-source/blob/master/',
+});
+exports.default = exports.ProjectSetting;
+//# sourceMappingURL=index.js.map
