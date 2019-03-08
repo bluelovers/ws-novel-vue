74134b6fbd70c9ed60f357a7fea133f9e5131657
bluelovers <codelovers@users.sourceforge.net>
2019-03-08 21:45:16 +0800

    試圖修正 build.json 更新錯誤的問題

--- script/build/build-cache.js
+++ script/build/build-cache.js
@@ -2,20 +2,26 @@
 /**
  * Created by user on 2019/2/2/002.
  */
-Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const novelStatJson = require("../../public/static/novel-stat.json");
 const fs = require("fs-extra");
 const project_config_1 = tslib_1.__importDefault(require("../../project.config"));
+const lib_1 = require("node-novel-info/lib");
 const path = require("path");
 const util_1 = tslib_1.__importDefault(require("../util"));
 const cache_loader_1 = require("@node-novel/cache-loader");
-let output = {
-    meta: novelStatJson.meta,
-    buildTimestamp: cache_loader_1.createMoment().valueOf(),
-};
-fs.outputJSONSync(path.join(project_config_1.default.ProjectRoot, 'src', 'setting', 'build.json'), output, {
-    spaces: '\t',
-});
-util_1.default.success(`[cache] build.json`);
+function cacheBuildJson() {
+    const novelStatJson = fs.readJSONSync(path.join(project_config_1.default.ProjectRoot, 'public/static/novel-stat.json'));
+    let output = {
+        meta: novelStatJson.meta,
+        buildTimestamp: cache_loader_1.createMoment().valueOf(),
+    };
+    fs.outputJSONSync(path.join(project_config_1.default.ProjectRoot, 'src', 'setting', 'build.json'), output, {
+        spaces: '\t',
+    });
+    util_1.default.success(`[cache] build.json`);
+    util_1.default.dir(output);
+    util_1.default.log('buildTimestamp', lib_1.moment(output.buildTimestamp).format());
+    util_1.default.log('meta.todayTimestamp', lib_1.moment(output.meta.todayTimestamp).format());
+}
+module.exports = cacheBuildJson();
 //# sourceMappingURL=build-cache.js.map
--- script/build/build-cache.ts
+++ script/build/build-cache.ts
@@ -2,7 +2,6 @@
  * Created by user on 2019/2/2/002.
  */
 
-import novelStatJson = require('../../public/static/novel-stat.json');
 import fs = require('fs-extra');
 import ProjectConfig from '../../project.config';
 import { moment } from 'node-novel-info/lib';
@@ -10,13 +9,25 @@ import path = require('path');
 import console from '../util';
 import { createMoment } from '@node-novel/cache-loader';
 
-let output = {
-	meta: novelStatJson.meta,
-	buildTimestamp: createMoment().valueOf(),
-};
+function cacheBuildJson()
+{
+	const novelStatJson = fs.readJSONSync(path.join(ProjectConfig.ProjectRoot, 'public/static/novel-stat.json')) as typeof import('../../public/static/novel-stat.json');
 
-fs.outputJSONSync(path.join(ProjectConfig.ProjectRoot, 'src', 'setting', 'build.json'), output, {
-	spaces: '\t',
-});
+	let output = {
+		meta: novelStatJson.meta,
+		buildTimestamp: createMoment().valueOf(),
+	};
 
-console.success(`[cache] build.json`);
+	fs.outputJSONSync(path.join(ProjectConfig.ProjectRoot, 'src', 'setting', 'build.json'), output, {
+		spaces: '\t',
+	});
+
+	console.success(`[cache] build.json`);
+
+	console.dir(output);
+
+	console.log('buildTimestamp', moment(output.buildTimestamp).format());
+	console.log('meta.todayTimestamp', moment(output.meta.todayTimestamp).format());
+}
+
+export = cacheBuildJson()
