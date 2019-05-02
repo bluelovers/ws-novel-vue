4e3fb488c6c68dcf76f423b366e12a883a12b745
bluelovers <codelovers@users.sourceforge.net>
2019-05-02 22:49:08 +0800

    yarn version

--- script/yarn-install.js
+++ script/yarn-install.js
@@ -2,16 +2,22 @@
 /**
  * Created by user on 2019/5/2.
  */
+const tslib_1 = require("tslib");
 const cross_spawn_extra_1 = require("cross-spawn-extra");
-module.exports = cross_spawn_extra_1.async('yarn', [
-    'add',
-    'regexp-cjk@latest',
-    'cjk-conv@latest',
-], {
-    stdio: 'inherit'
-})
-    .catch(e => {
-    console.error(e.message);
-    return null;
-});
+const util_1 = tslib_1.__importDefault(require("./util"));
+module.exports = (async () => {
+    let cmds = [
+        'add',
+        'regexp-cjk@latest',
+        'cjk-conv@latest',
+    ];
+    util_1.default.info(`yarn ${cmds.join(' ')}`);
+    await cross_spawn_extra_1.async('yarn', cmds, {
+        stdio: 'inherit'
+    })
+        .catch(e => {
+        util_1.default.error(e.message);
+        return null;
+    });
+})();
 //# sourceMappingURL=yarn-install.js.map
--- script/yarn-install.ts
+++ script/yarn-install.ts
@@ -4,16 +4,25 @@
 
 import { async as CrossSpawn } from 'cross-spawn-extra';
 import Bluebird = require('bluebird');
+import console, { awaitImport } from './util';
 
-export = CrossSpawn('yarn', [
-	'add',
-	'regexp-cjk@latest',
-	'cjk-conv@latest',
-], {
-	stdio: 'inherit'
-})
-	.catch(e => {
-		console.error(e.message);
-		return null
+export = (async () => {
+
+	let cmds = [
+		'add',
+		'regexp-cjk@latest',
+		'cjk-conv@latest',
+	];
+
+	console.info(`yarn ${cmds.join(' ')}`);
+
+	await CrossSpawn('yarn', cmds, {
+		stdio: 'inherit'
 	})
-;
+		.catch(e => {
+			console.error(e.message);
+			return null
+		})
+	;
+
+})();
